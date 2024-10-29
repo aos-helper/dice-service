31c18444e192857b08fc0c190de76898f2ac1c49
Marcin Lusa <mlooser@gmail.com>
2024-10-29 12:39:04 +0100

    add eureka registration

--- dice-service/src/main/java/aoshelper/diceservice/DiceServiceApplication.java
+++ dice-service/src/main/java/aoshelper/diceservice/DiceServiceApplication.java
@@ -3,6 +3,7 @@ package aoshelper.diceservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DiceServiceApplication {
 
